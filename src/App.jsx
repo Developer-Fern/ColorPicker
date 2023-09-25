68284861cf3cf5da3d1b2bcf98d0257a07761926
Fern <FernandoFlores.private@gmail.com>
2023-09-25 02:30:12 -0400

    added color component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
 // Write your Color component here
+import { useState } from 'react'
+
+const Color = props => {
+  return (
+    <div
+      className={
+        props.color === props.chosenColor
+          ? `${props.color} selected`
+          : props.color
+      }
+      // className={props.color}
+      onClick={() => props.selected(props.color)}
+    ></div>
+  )
+}
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState('')
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        {
+          <div id="colors-list">
+            <Color
+              color="blue"
+              selected={setSelectedColor}
+              chosenColor={selectedColor}
+            />
+            <Color
+              color="red"
+              selected={setSelectedColor}
+              chosenColor={selectedColor}
+            />
+            <Color
+              color="green"
+              selected={setSelectedColor}
+              chosenColor={selectedColor}
+            />
+          </div>
+        }
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
